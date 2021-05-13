5fe9901b4db24cc30fcbdba27639fc04d6ec5069
DangNguyen146 <19521317@gm.uit.edu.vn>
2021-05-13 20:48:18 +0700

    route

--- src/index.js
+++ src/index.js
@@ -7,10 +7,19 @@ const handlebars = require("express-handlebars");
 const app = express();
 const port = 3000;
 
+const route = require("./routes");
+
 app.use(express.static(path.join(__dirname, "public")));
 
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+app.use(express.json());
+
 //Http logger
-app.use(morgan("combined"));
+// app.use(morgan("combined"));
 //template engine
 app.engine(
   "hbs",
@@ -21,12 +30,7 @@ app.engine(
 app.set("view engine", "hbs");
 app.set("views", path.join(__dirname, "resources", "views"));
 
-app.get("/", (req, res) => {
-  res.render("home");
-});
-app.get("/news", (req, res) => {
-  res.render("news");
-});
+route(app);
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
---
+++ src/routes/index.js
@@ -0,0 +1,9 @@
+const newRouter = require("./news.route");
+const siteRoute = require("./site.route");
+
+function route(app) {
+  app.use("/news", newRouter);
+
+  app.use("/", siteRoute);
+}
+module.exports = route;
